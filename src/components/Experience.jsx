47d0b1cfa3e681d3739b85b5a41395d09e943c44
s7horizon <muhammedbashir384@gmail.com>
2025-07-27 00:02:07 +0100

    Added picture and underline effect to headings

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -5,7 +5,7 @@ function Experience() {
     <div className="flex flex-col items-center py-12 w-full px-4 sm:px-6 md:px-8 mb-45">
 
       <div className="text-center mb-10">
-        <h3 className="text-3xl sm:text-4xl md:text-5xl font-semibold">
+        <h3 className="text-3xl sm:text-4xl md:text-5xl font-semibold underline underline-offset-8">
           Work Experience
         </h3>
       </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -23,7 +23,7 @@ function Projects() {
 
   return (
     <div className="flex flex-col items-center py-12 px-4 sm:px-6 md:px-8 w-full">
-      <h3 className="text-3xl sm:text-4xl md:text-5xl mb-8 text-center">Projects</h3>
+      <h3 className="text-3xl sm:text-4xl md:text-5xl mb-8 text-center underline underline-offset-8 md-underline-offset-6">Projects</h3>
       <div className="flex flex-col sm:flex-row flex-wrap gap-8 justify-center items-center w-full">
         {projects.map((project, index) => (
           <ProjectCard key={index} project={project} />
