e70bdcc587624751988116224d4e05ad6df17fc0
s7horizon <muhammedbashir384@gmail.com>
2025-07-27 01:31:18 +0100

    Removed mb in stack

--- src/components/Stacks.jsx
+++ src/components/Stacks.jsx
@@ -98,6 +98,28 @@ function Stack() {
         </svg>
       ),
     },
+    {
+      name: "Bootstrap",
+      svg: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={640}
+          height={512}
+          viewBox="0 0 24 24"
+        >
+          <g
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={1.5}
+          >
+            <path d="M12.882 12c1.17 0 2.118.895 2.118 2s-.948 2-2.118 2H10.6c-.754 0-1.131 0-1.366-.234C9 15.53 9 15.154 9 14.4V12m3.882 0c1.17 0 2.118-.895 2.118-2s-.948-2-2.118-2H10.6c-.754 0-1.131 0-1.366.234C9 8.47 9 8.846 9 9.6V12m3.882 0H9"></path>
+            <path d="M22 12a2 2 0 0 1-2-2V8c0-3.31-.69-4-4-4H8c-3.31 0-4 .69-4 4v2a2 2 0 0 1-2 2m0 0a2 2 0 0 1 2 2v2c0 3.31.69 4 4 4h8c3.31 0 4-.69 4-4v-2a2 2 0 0 1 2-2"></path>
+          </g>
+        </svg>
+      ),
+    },
   ];
   return (
     <div className="flex flex-col items-center py-12 w-full px-4 sm:px-6 md:px-8">
